c7286a0a975ba988fc376050003ea58dde6ab1a8
HenriqueCosta05 <Henrique.b.costa9090@gmail.com>
2024-02-10 02:15:12 -0300

    ✨ feat: form validation and auth page - part 2

--- client/frontend/src/services/validations/FullName/validateFullName.ts
+++
@@ -1,6 +0,0 @@
-export function validateFullName(fullName: string): string | undefined {
-    if (!fullName) return "Please enter your full name.";
-    if (fullName.length < 5) return "Minimum length is 5 characters.";
-    if (!/^[a-zA-Z]+(?:\s[a-zA-Z]+)+$/.test(fullName)) return "Invalid full name format.";
-    return undefined;
-}
--- client/frontend/src/ui/components/auth/signin/signinform/FormSignIn.tsx
+++ client/frontend/src/ui/components/auth/signin/signinform/FormSignIn.tsx
@@ -1,23 +1,80 @@
 import React from 'react'
 import { Button, Form, FormControl, FormGroup, FormLabel } from 'react-bootstrap'
+import Feedback from 'react-bootstrap/esm/Feedback';
 import FormCheckInput from 'react-bootstrap/esm/FormCheckInput'
+import { Controller, SubmitHandler, useForm } from 'react-hook-form'
+
+interface FormFields {
+  email: string;
+  password: string;
+}
 
 export default function FormSignIn() {
+  const { control, handleSubmit, formState: { errors, isSubmitting }, register} = useForm<FormFields>({
+        defaultValues: {
+          email: "",
+          password: ""
+        }
+    })
+  const onSubmit: SubmitHandler<FormFields> = (data) => {
+    console.log(data);
+  }
   return (
     <>
-      <Form className='w-11/12'>
+      <Form className='w-11/12' onSubmit={handleSubmit(onSubmit)} noValidate>
         <FormGroup className="mt-3">
           <FormLabel className='mt-3'>E-mail Address: </FormLabel>
-          <FormControl type="email" placeholder="Enter email" />
+          <Controller
+            name='email'
+            control={control}
+            rules={{
+              required: "Please enter your email.",
+              pattern: {
+                value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                message: "Please enter a valid email address."
+              },
+            }}
+            render={({ field }) => (
+              <FormControl
+                {...field}
+                isInvalid={errors.email}
+                type="email"
+                placeholder="Enter email" />
+            )}
+          />
+          {errors.email && (
+            <Feedback type="invalid">{errors.email.message}</Feedback>
+          )}
+
           <FormLabel className='mt-3'>Password: </FormLabel>
-          <FormControl type="password" placeholder="Enter password" />
+          <Controller
+            name='password'
+            control={control}
+            rules={{
+              required: "Please enter your password.",
+              pattern: {
+                value: /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!-\/:-@[-`{-~]).{8,}$/,
+                message: "Password must contain at least 8 characters, including at least one letter and one number."
+              },
+            }}
+            render={({ field }) => (
+              <FormControl
+                {...field}
+                isInvalid={errors.password}
+                type="password"
+                placeholder="Enter password" />
+            )}
+          />
+          {errors.password && (
+            <Feedback type="invalid">{errors.password.message}</Feedback>
+          )}
           <div className="flex flex-nowrap h-auto mt-2 justify-start items-center">
             <FormCheckInput type="checkbox" />
             <FormLabel className='mt-3 ml-3'>Remember me</FormLabel>
           </div>
           <div className="flex flex-nowrap justify-center mt-8">
-            <Button href='/app' style={{width: "90%"}}>
-              Sign In to your account
+            <Button type="submit" disabled={isSubmitting} style={{width: "90%"}}>
+              {isSubmitting ? "Signing in..." : "Sign into your account"}
           </Button>
           </div>
         </FormGroup>
